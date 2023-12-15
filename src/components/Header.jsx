564401e3bd0c59bd096bdfabd1b64ed661a5eccc
Arturukis <752art71tur57@gmail.com>
2023-12-15 05:24:30 +0300

    small adjustments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,14 +7,14 @@ const Header = () => {
     return (
         <header>
             <nav>
-                <BsFillStarFill style={{fontSize: '5.5vw'}}/>
+
                 <ButtonGroup variant="contained" aria-label="outlined primary button group" className="roundedButtonGroup">
                     <Button id = 'ButtonStyle'>Главная</Button>
                     <Button id = 'ButtonStyle'>Деятель дня</Button>
                     <Button id = 'ButtonStyle'>Деятели</Button>
                     <Button id = 'ButtonStyle'>О нас</Button>
                 </ButtonGroup>
-                <BsFillStarFill style={{fontSize: '5.6vw'}}/>
+
             </nav>
         </header>
     );
