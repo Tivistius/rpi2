318e40e60cf16bfba682564430979f01db9216ae
Arturukis <752art71tur57@gmail.com>
2023-12-15 05:04:36 +0300

    Full header without links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { BsFillStarFill } from 'react-icons/bs';
 import '../styles/Header.css';
+import { Button, ButtonGroup } from '@mui/material';
 
 const Header = () => {
     return (
         <header>
             <nav>
-                <ul>
-                    <li>
-                        <BsFillStarFill />
-                    </li>
-                    <li><a href="/">Главная</a></li>
-                    <li><a href="/about"> Деятель дня</a></li>
-                    <li><a href="/contact"> Деятели</a></li>
-                    <li><a href="/">О нас</a></li>
-                </ul>
+                <BsFillStarFill style={{fontSize: '5.5vw'}}/>
+                <ButtonGroup variant="contained" aria-label="outlined primary button group" className="roundedButtonGroup">
+                    <Button id = 'ButtonStyle'>Главная</Button>
+                    <Button id = 'ButtonStyle'>Деятель дня</Button>
+                    <Button id = 'ButtonStyle'>Деятели</Button>
+                    <Button id = 'ButtonStyle'>О нас</Button>
+                </ButtonGroup>
+                <BsFillStarFill style={{fontSize: '5.6vw'}}/>
             </nav>
         </header>
     );
