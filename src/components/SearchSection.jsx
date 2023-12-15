e8567669b8370b8b629217c61118356f196add52
Arturukis <752art71tur57@gmail.com>
2023-12-15 06:10:58 +0300

    small adjustments

--- src/components/SearchSection.jsx
+++ src/components/SearchSection.jsx
@@ -7,13 +7,13 @@ const SearchSection = () => {
     return (
         <div className= 'SearchSection'>
             <img className= 'Logo'
-                src="/images/Logo2.svg.png"
+                src="/images/postav_logo.png"
                 alt=":("
             />
             <div className= 'SearchContainer'>
                 <div className= 'LanguageChanger'>
-                    <Button variant="contained" style={{ width: '22%', height: '70%', fontSize: '2.5vh', padding: '0', minWidth: '0', minHeight: '0', borderRadius: '2vw' }}>Eng</Button>
-                    <Button variant="contained" style={{ width: '22%', height: '70%', fontSize: '2.5vh', padding: '0', minWidth: '0', minHeight: '0', borderRadius: '2vw'}}>Рус</Button>
+                    <Button variant="contained" style={{ width: '22%', height: '90%', fontSize: '2.1vw', padding: '0', minWidth: '0', minHeight: '0', borderRadius: '2vw' }}>Eng</Button>
+                    <Button variant="contained" style={{ width: '22%', height: '90%', fontSize: '2.1vw', padding: '0', minWidth: '0', minHeight: '0', borderRadius: '2vw'}}>Рус</Button>
                 </div>
 
                 <Search>
