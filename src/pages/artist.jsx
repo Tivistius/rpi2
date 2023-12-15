70c1c3d89b9d97fb932d729b5460ec17efe463a2
Minduction <dima.filistovich.05@mail.ru>
2023-12-16 02:30:09 +0300

    almost finished allrtists

--- src/pages/artist.jsx
+++ src/pages/artist.jsx
@@ -2,11 +2,22 @@ import React, {useState} from 'react';
 import artistArray from '../test_data/test_data.js'; // Путь к файлу test_data.js
 import '../styles/artist.css'
 import ArtistAllInfo from "../components/artistAllInfo";
-import {readFile} from "fs";
-
+import axios from 'axios';
 
 const Artist = () => {
-    const [artists, setArtists] = useState([data[0]]);
+
+    const путьКФайлу = '../images/info.json';
+
+    axios.get(путьКФайлу)
+        .then(response => {
+            const jsonОбъект = response.data;
+            console.log(jsonОбъект);
+        })
+        .catch(ошибка => {
+            console.error('Ошибка чтения файла:', ошибка);
+        });
+
+    const [artists, setArtists] = useState([artistArray[0]]);
     return (
 
         <div>
