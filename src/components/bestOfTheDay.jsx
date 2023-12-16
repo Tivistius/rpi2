56ce1cac2aa36cd0193d61631eed0a1217668ca8
Arturukis <752art71tur57@gmail.com>
2023-12-16 13:54:48 +0300

    something

---
+++ src/components/bestOfTheDay.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BestOfTheDay = (props) => {
+   console.log(props);
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default BestOfTheDay;
---
+++ src/pages/PageAbout.jsx
@@ -0,0 +1,38 @@
+import React, {useEffect, useState} from 'react';
+import About from "../components/About";
+import BestOfTheDay from "../components/bestOfTheDay";
+import axios from "axios";
+
+const fetchData = async () => {
+    try {
+        const response = await axios.get('../images/info.json');
+        const temp = response.data;
+        const currentDate = new Date();
+        const dayOfWeek = currentDate.getDay();
+        const randomValue = (dayOfWeek + 1) % 5;
+        const toShow = temp[randomValue];
+
+        return toShow;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return null;
+    }
+};
+
+const PageAbout = () => {
+    const [dataToShow, setDataToShow] = useState(null);
+
+    useEffect(() => {
+        fetchData().then(result => {
+            setDataToShow(result);
+        });
+    }, []);
+    return (
+        <div>
+            <About />
+            {dataToShow && <BestOfTheDay toShow={dataToShow} />}
+        </div>
+    );
+};
+
+export default PageAbout;
