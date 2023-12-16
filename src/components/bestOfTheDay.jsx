f444aa80f4789e3dd54f6d4edf2d0c95d9fb4995
Arturukis <752art71tur57@gmail.com>
2023-12-16 18:07:06 +0300

    some additions

--- src/components/bestOfTheDay.jsx
+++ src/components/bestOfTheDay.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/BestOfTheDay.css';
+import {Button, ThemeProvider} from "@mui/material";
 
 const BestOfTheDay = (props) => {
    console.log(props);
@@ -12,12 +13,24 @@ const BestOfTheDay = (props) => {
                 <h3 className = 'Title'>
                     Деятель дня
                 </h3>
+                <h3 className = 'Name'>
+                    {fio}
+                </h3>
                 <div className = 'PhotoPart'>
                     <img src={photoPath} alt=":(" className = 'ImageStyle'/>
                     <div className = 'Date'>
-                        {bornDate} - {deathDate}
+                        {bornDate} — {deathDate}
+                    </div>
+                    <p className={"shortDescription"}>{shortInfo}</p>
+                    <div className={"buttonMore"}>
+                        <Button variant="contained" color="secondary" className ='Button'>
+                            <h1 style={{margin: '0px',fontSize: '2vw'}}>ПОДРОБНЕЕ</h1>
+                        </Button>
+
                     </div>
                 </div>
+
+
 
             </div>
         </div>
